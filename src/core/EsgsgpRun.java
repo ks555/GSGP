67e3f1e69aa1c346361ed473c9858e3cf1ea7666
ks555 <kristenmscott@hotmail.com>
2016-10-05 10:41:32 +0100

    Changed console printing to Mindividuals

--- src/core/EsgsgpRun.java
+++ src/core/EsgsgpRun.java
@@ -43,23 +43,24 @@ public class EsgsgpRun extends GsgpRun {
 			
 			for (int j = 0; j < numPrograms; j++) {
 				mindividual.addProgramAtIndex(populations.get(j).getIndividual(i),j);			
-				System.out.println("Initialized Individual " + i + ":" + j + " ID: " + populations.get(j).getIndividual(i).getId()
-						+ "\n" + "Training Error: " + populations.get(j).getIndividual(i).trainingError 
-						+ "\n" + "Unseen Error: " + populations.get(j).getIndividual(i).unseenError);					
+//				System.out.println("Initialized Individual " + i + ":" + j + " ID: " + populations.get(j).getIndividual(i).getId()
+//						+ "\n" + "Training Error: " + populations.get(j).getIndividual(i).trainingError 
+//						+ "\n" + "Unseen Error: " + populations.get(j).getIndividual(i).unseenError);					
 			}
 			mindividual.evaluate();
 			mpopulation.addIndividual(mindividual);			
-			System.out.println("Mindividual Error Averages for ID: " + mindividual.getId()+"\nTraining Error " 
-			+ mindividual.getTrainingError() + "\nUnseen Error Total " + mindividual.getUnseenError()+"\n");
+//			System.out.println("Mindividual Error Averages for ID: " + mindividual.getId()+"\nTraining Error " 
+//			+ mindividual.getTrainingError() + "\nUnseen Error Total " + mindividual.getUnseenError()+"\n");
 			
 		}
 	
 		applyDepthLimit = false;
 		mutationStep = 1.0;
-		boundedMutation = false;
-		//boundedMutation = true;
+		//boundedMutation = false;
+		boundedMutation = true;
 		//buildIndividuals = true;
 		buildIndividuals = false;
+		
 		
 		
 	}
@@ -102,18 +103,28 @@ public class EsgsgpRun extends GsgpRun {
 				} else {
 					for (int i=0; i<numPrograms; i++){
 						newIndividual.getProgram(i).evaluate(data);
+						
 					}
+					//System.out.println("Offspring first program training error " +newIndividual.getProgram(0).getTrainingError());
 					newIndividual.evaluate();
 				}
 				offspring.addIndividual(newIndividual);
-				System.out.println(newIndividual.getTrainingError()+"\n");
+				//System.out.println("offspring training " +newIndividual.getTrainingError()+"\n");
 			}
 
-			population = selectSurvivors(offspring);
-			updateCurrentMBest();
+			mpopulation = selectSurvivors(offspring);
 			
-			printState();
+			updateCurrentMBest();
+			printMPopState();
 			currentGeneration++;
+		}
+	}
+	
+	protected void printMPopState() {
+		if (printAtEachGeneration) {
+			System.out.println("\nGeneration:\t\t" + currentGeneration);
+			System.out.printf("Training error:\t\t%.2f\nUnseen error:\t\t%.2f\n",
+					currentMBest.getTrainingError(), currentMBest.getUnseenError());
 		}
 	}
 	
@@ -135,6 +146,9 @@ public class EsgsgpRun extends GsgpRun {
 	
 	protected MIndividual applyStandardCrossover(MIndividual mp1, MIndividual mp2) {
 		MIndividual offspring = new MIndividual();
+		if (buildIndividuals == false) {
+			offspring.setSizeOverride(true);
+		}
 		//for each program in numPrograms
 		//choose random program from mp1 and mp2 as 'parents' of the new program.
 		//add each program 'offspring' to the final offspring (an MIndividual)
@@ -148,19 +162,21 @@ public class EsgsgpRun extends GsgpRun {
 				
 			if (buildIndividuals) {
 				pOffspring = buildCrossoverIndividual(p1, p2);
+				
 				offspring.addProgramAtIndex(pOffspring, i);
 			}
 			else {
 				pOffspring = buildCrossoverSemantics(p1, p2);
+				
 				offspring.addProgramAtIndex(pOffspring,i);
 			}	
 			
 			
 		}	
-		//print 
-		System.out.print("offspring ID: " +offspring.getId());
-		System.out.println(mp1.getTrainingError());
-		System.out.println(mp2.getTrainingError());
+		//print
+//		System.out.println("offspring ID: " +offspring.getId());
+//		System.out.println("P1 training error " + mp1.getTrainingError());
+//		System.out.println("P2 training error " + mp2.getTrainingError());
 		
 		
 		
@@ -168,100 +184,7 @@ public class EsgsgpRun extends GsgpRun {
 	}
 
 	
-//	protected Individual buildCrossoverIndividual(Individual p1, Individual p2) {
-//
-//		Individual offspring = new Individual();
-//
-//		offspring.addProgramElement(new Addition());
-//		offspring.addProgramElement(new Multiplication());
-//
-//		// copy first parent to offspring
-//		for (int i = 0; i < p1.getSize(); i++) {
-//			offspring.addProgramElement(p1.getProgramElementAtIndex(i));
-//		}
-//
-//		// create a random tree
-//		int maximumInitialDepth = 6;
-//		Individual randomTree = grow(maximumInitialDepth);
-//
-//		offspring.addProgramElement(new LogisticFunction());
-//		// copy random tree to offspring
-//		for (int i = 0; i < randomTree.getSize(); i++) {
-//			offspring.addProgramElement(randomTree.getProgramElementAtIndex(i));
-//		}
-//
-//		offspring.addProgramElement(new Multiplication());
-//		offspring.addProgramElement(new Subtraction());
-//		offspring.addProgramElement(new Constant(1.0));
-//
-//		offspring.addProgramElement(new LogisticFunction());
-//		// copy random tree to offspring
-//		for (int i = 0; i < randomTree.getSize(); i++) {
-//			offspring.addProgramElement(randomTree.getProgramElementAtIndex(i));
-//		}
-//
-//		// copy second parent to offspring
-//		for (int i = 0; i < p2.getSize(); i++) {
-//			offspring.addProgramElement(p2.getProgramElementAtIndex(i));
-//		}
-//
-//		offspring.calculateDepth();
-//		return offspring;
-//	}
 
-//	protected Individual buildCrossoverSemantics(Individual p1, Individual p2) {
-//
-//		Individual offspring = new Individual();
-//
-//		// create a random tree and evaluate it
-//		int maximumInitialDepth = 6;
-//		Individual randomTree = grow(maximumInitialDepth);
-//		randomTree.evaluate(data);
-//
-//		// build training data semantics
-//		double[] parent1TrainingSemantics = p1.getTrainingDataOutputs();
-//		double[] parent2TrainingSemantics = p2.getTrainingDataOutputs();
-//		double[] randomTreeTrainingSemantics = randomTree.getTrainingDataOutputs();
-//		double[] offspringTrainingSemantics = buildCrossoverOffspringSemantics(parent1TrainingSemantics,
-//				parent2TrainingSemantics, randomTreeTrainingSemantics);
-//		offspring.setTrainingDataOutputs(offspringTrainingSemantics);
-//
-//		// build unseen data semantics
-//		double[] parent1UnseenSemantics = p1.getUnseenDataOutputs();
-//		double[] parent2UnseenSemantics = p2.getUnseenDataOutputs();
-//		double[] randomTreeUnseenSemantics = randomTree.getUnseenDataOutputs();
-//		double[] offspringUnseenSemantics = buildCrossoverOffspringSemantics(parent1UnseenSemantics,
-//				parent2UnseenSemantics, randomTreeUnseenSemantics);
-//		offspring.setUnseenDataOutputs(offspringUnseenSemantics);
-//
-//		// calculate size and depth
-//		offspring.setSizeOverride(true);
-//		offspring.setComputedSize(calculateCrossoverOffspringSize(p1, p2, randomTree));
-//		offspring.setDepth(calculateCrossoverOffspringDepth(p1, p2, randomTree));
-//
-//		return offspring;
-//	}
-
-//	protected double[] buildCrossoverOffspringSemantics(double[] parent1Semantics, double[] parent2Semantics,
-//			double[] randomTreeSemantics) {
-//		double[] offspringSemantics = new double[parent1Semantics.length];
-//		for (int i = 0; i < offspringSemantics.length; i++) {
-//			double randomTreeValue = Utils.logisticFunction(randomTreeSemantics[i]);
-//			offspringSemantics[i] = (parent1Semantics[i] * randomTreeValue)
-//					+ ((1.0 - randomTreeValue) * parent2Semantics[i]);
-//		}
-//		return offspringSemantics;
-//	}
-//
-//	protected int calculateCrossoverOffspringSize(Individual p1, Individual p2, Individual randomTree) {
-//		return p1.getSize() + p2.getSize() + randomTree.getSize() * 2 + 5;
-//	}
-//
-//	protected int calculateCrossoverOffspringDepth(Individual p1, Individual p2, Individual randomTree) {
-//		int largestParentDepth = Math.max(p1.getDepth(), p2.getDepth());
-//		// "+ 1" because of the bounding function
-//		return Math.max(largestParentDepth + 2, randomTree.getDepth() + 3 + 1);
-//	}
 
 	protected MIndividual applyStandardMutation(MIndividual mp) {
 		MIndividual offspring = new MIndividual();
@@ -276,103 +199,7 @@ public class EsgsgpRun extends GsgpRun {
 		return offspring;
 	}
 
-//	protected Individual buildMutationIndividual(Individual p) {
-//
-//		Individual offspring = new Individual();
-//		offspring.addProgramElement(new Addition());
-//
-//		// copy parent to offspring
-//		for (int i = 0; i < p.getSize(); i++) {
-//			offspring.addProgramElement(p.getProgramElementAtIndex(i));
-//		}
-//
-//		offspring.addProgramElement(new Multiplication());
-//		offspring.addProgramElement(new Constant(mutationStep));
-//		offspring.addProgramElement(new Subtraction());
-//
-//		// create 2 random trees
-//		int maximumInitialDepth = 6;
-//		Individual randomTree1 = grow(maximumInitialDepth);
-//		Individual randomTree2 = grow(maximumInitialDepth);
-//
-//		if (boundedMutation) {
-//			offspring.addProgramElement(new LogisticFunction());
-//		}
-//		// copy random tree 1 to offspring
-//		for (int i = 0; i < randomTree1.getSize(); i++) {
-//			offspring.addProgramElement(randomTree1.getProgramElementAtIndex(i));
-//		}
-//
-//		if (boundedMutation) {
-//			offspring.addProgramElement(new LogisticFunction());
-//		}
-//		// copy random tree 2 to offspring
-//		for (int i = 0; i < randomTree2.getSize(); i++) {
-//			offspring.addProgramElement(randomTree2.getProgramElementAtIndex(i));
-//		}
-//
-//		offspring.calculateDepth();
-//		return offspring;
-//	}
-//
-//	protected Individual buildMutationSemantics(Individual p) {
-//
-//		Individual offspring = new Individual();
-//
-//		// create 2 random trees and evaluate them
-//		int maximumInitialDepth = 6;
-//		Individual randomTree1 = grow(maximumInitialDepth);
-//		Individual randomTree2 = grow(maximumInitialDepth);
-//		randomTree1.evaluate(data);
-//		randomTree2.evaluate(data);
-//
-//		// build training data semantics
-//		double[] parentTrainingSemantics = p.getTrainingDataOutputs();
-//		double[] randomTree1TrainingSemantics = randomTree1.getTrainingDataOutputs();
-//		double[] randomTree2TrainingSemantics = randomTree2.getTrainingDataOutputs();
-//		double[] offspringTrainingSemantics = buildMutationOffspringSemantics(parentTrainingSemantics,
-//				randomTree1TrainingSemantics, randomTree2TrainingSemantics);
-//		offspring.setTrainingDataOutputs(offspringTrainingSemantics);
-//
-//		// build unseen data semantics
-//		double[] parentUnseenSemantics = p.getUnseenDataOutputs();
-//		double[] randomTree1UnseenSemantics = randomTree1.getUnseenDataOutputs();
-//		double[] randomTree2UnseenSemantics = randomTree2.getUnseenDataOutputs();
-//		double[] offspringUnseenSemantics = buildMutationOffspringSemantics(parentUnseenSemantics,
-//				randomTree1UnseenSemantics, randomTree2UnseenSemantics);
-//		offspring.setUnseenDataOutputs(offspringUnseenSemantics);
-//
-//		// calculate size and depth
-//		offspring.setSizeOverride(true);
-//		offspring.setComputedSize(calculateMutationOffspringSize(p, randomTree1, randomTree2));
-//		offspring.setDepth(calculateMutationOffspringDepth(p, randomTree1, randomTree2));
-//
-//		return offspring;
-//	}
-//
-//	protected double[] buildMutationOffspringSemantics(double[] parentSemantics, double[] randomTree1Semantics,
-//			double[] randomTree2Semantics) {
-//		double[] offspringSemantics = new double[parentSemantics.length];
-//		for (int i = 0; i < offspringSemantics.length; i++) {
-//			double value1 = randomTree1Semantics[i];
-//			double value2 = randomTree2Semantics[i];
-//			if (boundedMutation) {
-//				value1 = Utils.logisticFunction(value1);
-//				value2 = Utils.logisticFunction(value2);
-//			}
-//			offspringSemantics[i] = parentSemantics[i] + (mutationStep * (value1 - value2));
-//		}
-//		return offspringSemantics;
-//	}
-//
-//	protected int calculateMutationOffspringSize(Individual parent, Individual randomTree1, Individual randomTree2) {
-//		return parent.getSize() + randomTree1.getSize() + randomTree2.getSize() + 4;
-//	}
-//
-//	protected int calculateMutationOffspringDepth(Individual parent, Individual randomTree1, Individual randomTree2) {
-//		int largestRandomTreeDepth = Math.max(randomTree1.getDepth(), randomTree2.getDepth());
-//		return Math.max(largestRandomTreeDepth + 3, parent.getDepth() + 1);
-//	}	
+
 	
 	// keep the best overall + all the remaining offsprings
 	protected MPopulation selectSurvivors(MPopulation newIndividuals) {
@@ -380,21 +207,28 @@ public class EsgsgpRun extends GsgpRun {
 		MIndividual bestParent = mpopulation.getBestM();
 		MIndividual bestNewIndividual = newIndividuals.getBestM();
 		MIndividual bestOverall;
+		
+		
+
 		// the best overall is in the current population
 		if (bestParent.getTrainingError() < bestNewIndividual.getTrainingError()) {
+			//System.out.println("Best parent" +bestParent.getTrainingError());
 			bestOverall = bestParent;
 		}
 		// the best overall is in the offspring population
 		else {
 			bestOverall = bestNewIndividual;
+			//System.out.println("Best  new" +bestNewIndividual.getTrainingError());
 		}
 
 		survivors.addIndividual(bestOverall);
+		//System.out.println("Best overall" +bestOverall.getTrainingError());
 		for (int i = 0; i < newIndividuals.getSize(); i++) {
 			if (newIndividuals.getMIndividual(i).getId() != bestOverall.getId()) {
 				survivors.addIndividual(newIndividuals.getMIndividual(i));
 			}
 		}
+		//System.out.print("Survivors best: " + survivors.getBestM().getTrainingError());
 		return survivors;
 	}
 	
@@ -404,7 +238,9 @@ public class EsgsgpRun extends GsgpRun {
 	}
 
 	// ##### get's and set's from here on #####
-
+	public MIndividual getCurrentMBest() {
+		return currentMBest;
+	}
 	public double getMutationStep() {
 		return mutationStep;
 	}
